0dacd372555c9862fdfa2ff4ba24c225e3e8e1d1
Lin <lin.guzman@tecsup.edu.pe>
2024-10-02 16:42:17 -0500

    Initial Commit

---
+++ GuzmanColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuzmanColeccionDeJuegos
+//
+//  Created by Lin Abigail Guzman Gutierrez on 2/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
